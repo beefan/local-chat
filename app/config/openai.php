30d600365b829bf37246751d858ebbbe6f362427
Brandon Fannin <bfannin13@gmail.com>
2024-09-28 14:55:35 -0400

    configure model by env var

--- app/config/openai.php
+++ app/config/openai.php
@@ -4,7 +4,7 @@ return [
   'connectionTimeoutSeconds' => 60 * 10,
   'responseTimeoutSeconds' => 60 * 10,
   'chatCompletionEndpoint' => 'https://api.openai.com/v1/chat/completions',
-  'model' => 'gpt-4o',
+  'model' => env('OPENAI_MODEL', 'gpt-3.5-turbo'),
   'apiKey' => env('OPENAI_API_KEY'),
   'testMode' => env('OPENAI_TEST_MODE', false),
 ];
